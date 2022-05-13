8ff264145a105cc4df338ccb8f53586af5ec00f0
Lucas Himschoot <34957838+WatermelonArray@users.noreply.github.com>
2022-05-13 02:11:35 +0100

    Added mobile navbar functionality

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 // Import libraries
-import React from "react";
+import React, {useRef} from "react";
 import { Outlet, Link } from "react-router-dom";
 
 import svgLogo from "../static/svgs/logo.svg";
@@ -9,16 +9,44 @@ import svgProfile from "../static/images/image-avatar.png";
 
 import "../static/css/navbar.css"
 
+let navbarOpen = false;
 
-function Navbar() {
+
+
+
+const Navbar = () => {
+
+	const pageLinkRef = useRef(null);
+	const hamburgerRef = useRef(null);
+
+	function toggleNavbar() {
+		navbarOpen = !navbarOpen
+		if (navbarOpen === false) {
+			pageLinkRef.current.className = "pageLinksClose"
+			hamburgerRef.current.style.fill = "black";
+		}
+		else {
+			pageLinkRef.current.className = "pageLinks"
+			hamburgerRef.current.style.fill = "hsl(223, 64%, 98%)"
+		}
+	}
+	function closeNavbar() {
+		navbarOpen = false;
+		pageLinkRef.current.className = "pageLinksClose"
+		hamburgerRef.current.style.fill = "black";
+	}
+
 	return (
-		<>
+		<>	
+			<style>
+				@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&display=swap');
+			</style>
+
 			<div className="navbarOffset" />
+
 			<nav className="navbar">
-				<style>
-					@import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&display=swap');
-				</style>
-				<button className="hamburger">
+
+				<button ref={hamburgerRef} onClick={toggleNavbar} className="hamburger">
 					<svg width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M16 12v3H0v-3h16Zm0-6v3H0V6h16Zm0-6v3H0V0h16Z" fill-rule="evenodd"/></svg>
 				</button>
 				<div className="navbarLogo">
@@ -26,15 +54,20 @@ function Navbar() {
 						<img src={svgLogo} alt="sneakers"></img>
 					</Link>
 				</div>
-				<ul className="pageLinks">
-					<li>
-						<Link className="topbarLinks" to="/collections">Collections</Link>
+
+				<ul ref={pageLinkRef} className="pageLinksClose">
+					<li className="topbarLinks topbarLinksHome">
+						<Link onClick={closeNavbar} className="topbarLinksStyle" to="/">Home</Link>
 					</li>
-					<li className="topbarLinks">Men</li>
-					<li className="topbarLinks">Women</li>
-					<li className="topbarLinks">About</li>
-					<li className="topbarLinks">Contact</li>
+					<li className="topbarLinks">
+						<Link onClick={closeNavbar} className="topbarLinksStyle" to="/collections">Collections</Link>
+					</li>
+					<li onClick={closeNavbar} className="topbarLinks topbarLinksStyle">Men</li>
+					<li onClick={closeNavbar} className="topbarLinks topbarLinksStyle">Women</li>
+					<li onClick={closeNavbar} className="topbarLinks topbarLinksStyle">About</li>
+					<li onClick={closeNavbar} className="topbarLinks topbarLinksStyle">Contact</li>
 				</ul>
+
 				<ul className="rightContent">
 					<li className="topbarRight">
 						<Link className="topbarText" to="/collections">
